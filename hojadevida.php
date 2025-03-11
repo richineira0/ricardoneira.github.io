fe4dc54a50f4e8dbc22ae9724487c881edbb9372
richineira0 <yassebh.neira01@usa.edu.co>
2025-03-11 12:38:13 -0500

    Add files via upload

---
+++ hojadevida.php
@@ -0,0 +1,95 @@
+<html>
+
+<head></head>
+
+<body>
+
+<?php
+
+echo"Hoja de Vida";
+
+echo "<br>";
+
+echo "Nombre: Sara Ximena Lopez Fonseca";
+
+echo "<br>";
+
+echo "Edad: 17";
+
+echo "<br>";
+
+echo "Fecha de Nacimiento: 26/02/2008";
+
+echo "<br>";
+
+echo "Coontacto: 1234567891";
+
+echo "<br>";
+
+echo "Correo electronico: sara.lopez03@usa.edu.co";
+
+echo "<br>";
+
+echo "Residencia: Bogota";
+
+echo "<br>";
+
+echo "Experiencia: Ninguna";
+
+echo "<br>";
+
+echo "Perfil Profesional: Habilidades de escucha, comunicacion, pensamiento critico ";
+
+echo "<br>";
+
+echo "Titulo Obtenido: Bachiller Academico";
+
+echo "<br>";
+
+echo "Habilidades Tecnicas: Excel Avanzado, Analisis Financiero";
+
+echo "<br>";
+
+echo "Habilidades blandas: Liderazgo, trabajo en equipo";
+
+echo "<br>";
+
+echo "Intereses: Crear empresa propia";
+
+echo "<br>";
+
+echo "Disonibilidad para viajar o cambiar de residencia";
+
+echo "<br>";
+
+echo "Alergias: Polvo";
+
+echo "<br>";
+
+echo "EPS: Sanitas";
+
+echo "<br>";
+
+echo "Mascotas: Dos gatos";
+
+echo "<br>";
+
+echo "Enfermedades: Asma, rinitis";
+
+echo "<br>";
+
+echo "Recomendaciones: Francis Gomez";
+
+echo "<br>";
+
+echo "Contacto: 3256985425";
+
+echo "<br>";
+
+echo "Estudios profesionales: Cursando ingenieria Industrial";
+
+?>
+
+</body>
+
+</html>
