7fc750169b997040599f20fdf2b414a7c6063e8e
richineira0 <yassebh.neira01@usa.edu.co>
2025-03-11 12:32:04 -0500

    Add files via upload

---
+++ primertrabajo.php
@@ -0,0 +1,97 @@
+<html>
+
+<head></head>
+
+<body>
+
+<?php
+
+echo "Bienvenido a clase de programación";
+
+echo "<br>";
+
+echo "Mi nombre es Yassebh Ricardo Neira Gomez"; 
+
+echo "<br>";
+
+echo "Tengo 17 años y nací el 19 de Julio del 2007";
+
+echo "<br>";
+
+echo "Vivo cerca al Portal 80";
+
+echo "<br>";
+
+echo "Estudio Ingenieria industrial en la Universidad Sergio Arboleda";
+
+echo "<br>";
+
+echo "Me gusta mucho hacer deporte y compartir tiempo de calidad con mis amigos";
+
+echo "<br>";
+
+echo "Mi color favorito es el morado y el rojo";
+
+echo "<br>";
+
+echo "Mi materia favorita en la Universidad es Dibujo digitalizado";
+
+echo "<br>";
+
+echo "He viajado a San Andres con mi familia";
+
+echo "<br>";
+
+echo "Me gusta mucho el circuit y la salsa cumbiambera";
+
+echo "<br>";
+
+echo "Odio montar en transmilenio, ya quiero tener mi propio carro";
+
+echo "<br>";
+
+echo "Mi comida favorita es la de mar";
+
+echo "<br>";
+
+echo "Cuando era pequeño me atore con un Bombombun y casi me ahogo por completo";
+
+echo "<br>";
+
+echo "Se mover las orejas";
+
+echo "<br>";
+
+echo "He trabajado como vendedor de ropa en el centro de Bogotá durante temporada";
+
+echo "<br>";
+
+echo "Mi fruta favorita es el Zapote";
+
+echo "<br>";
+
+echo "Mi lugar favorito es Medellín";
+
+echo "<br>";
+
+echo "Tengo dos gatos, uno se llama Zimba y el otro Kiara";
+
+echo "<br>";
+
+echo "Estudie ingles en el Colombo";
+
+echo "<br>";
+
+echo "Mi red social favorita es Instagram";
+
+echo "<br>";
+
+echo "A futuro quisiera tener mi propia empresa y viajar alrededor del mundo"
+
+?>
+
+</body>
+
+
+</html>
+
